32b67b5c57e7d3e60de48b9b60abad081077fe0e
newellworldorder <bwongggg@gmail.com>
2015-08-14 16:00:09 -0400

    changed youtube handling

--- URLInfo.py
+++ URLInfo.py
@@ -18,23 +18,24 @@ def youtubeKey():
         return None
 
 def youtube(self,Log,url):
-    vidID = url.split('youtu.be/')[-1].split('youtube.com/watch?v=')[-1].split('youtube.com/v/')[-1].split('#')[0].split('&')[0].split('?')[0]
-    try:
-        payload = {'part': 'snippet,statistics', 'id': vidID, 'key': youtubeKey()}
-        r = requests.get('https://www.googleapis.com/youtube/v3/videos', params = payload)
-        data = r.json()
-        title = data['items'][0]['snippet']['title']
-        channel = data['items'][0]['snippet']['channelTitle']
-        likes = int(data['items'][0]['statistics']['likeCount'])
-        dislikes = int(data['items'][0]['statistics']['dislikeCount'])
-        votes = likes + dislikes
-        if likes and dislikes:
-            bar = '12' + str(likes) + ' ' + '—' * round(likes*10/votes) + '15' + '—' * round(dislikes*10/votes) + ' ' + str(dislikes)
-        else:
-            bar = ''
-        self.PRIVMSG(Log['context'],'You00,04Tube %s 14uploaded by %s – %s' % (title, channel, bar))
-    except Exception as e:
-        print(e)
+    if 'youtube.com/' in url or 'youtu.be/' in url:
+        vidID = url.split('youtu.be/')[-1].split('v=')[-1].split('youtube.com/v/')[-1].split('#')[0].split('&')[0].split('?')[0]
+        try:
+            payload = {'part': 'snippet,statistics', 'id': vidID, 'key': youtubeKey()}
+            r = requests.get('https://www.googleapis.com/youtube/v3/videos', params = payload)
+            data = r.json()
+            title = data['items'][0]['snippet']['title']
+            channel = data['items'][0]['snippet']['channelTitle']
+            likes = int(data['items'][0]['statistics']['likeCount'])
+            dislikes = int(data['items'][0]['statistics']['dislikeCount'])
+            votes = likes + dislikes
+            if likes and dislikes:
+                bar = '12' + str(likes) + ' ' + '—' * round(likes*10/votes) + '15' + '—' * round(dislikes*10/votes) + ' ' + str(dislikes)
+            else:
+                bar = ''
+            self.PRIVMSG(Log['context'],'You00,04Tube %s 14uploaded by %s – %s' % (title, channel, bar))
+        except Exception as e:
+            print(e)
 sites['youtu.be/'] = youtube
 sites['youtube.com/'] = youtube
 
